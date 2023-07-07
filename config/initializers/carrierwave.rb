814188f4502a9fa30b30107f2479705fcd69b13a
nakata59 <yuusuke6121@docomo.ne.jp>
2023-07-08 02:19:56 +0900

    070802

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,18 +4,24 @@ require 'carrierwave/storage/fog'
 
 
 CarrierWave.configure do |config|
-
+  if Rails.env.production?
+    p "テスト"
+    p "#{ENV['AWS_ACCESS_KEY_ID']}"
+    p "ENV['AWS_SECRET_ACCESS_KEY']"
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'AKIAWOZ74Q4CSWUAC3TE',
-    aws_secret_access_key: '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',
-    region: 'ap-northeast-1'
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
     }
-
 
     config.fog_directory  = 'karuta-app'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/karuta-app'
-
+  else
+    # 開発環境はlocalに保存
+    config.storage :file
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end
 end
